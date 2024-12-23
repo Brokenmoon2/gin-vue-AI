201d4ec37d9d644d8293d1b4cd9a1ed61712a75b
Brokenmoon2 <1941658307@qq.com>
2024-12-23 09:25:34 +0800

    tabs路由切换,关闭,持久化

--- src/components/admin/gvb_tabs.vue
+++ src/components/admin/gvb_tabs.vue
@@ -1,12 +1,112 @@
 <template>
   <div class="gvb_tabs">
-    <span class="gvb_tab active">首页</span>
-    <span class="gvb_tab">用户列表</span>
-    <span class="gvb_tab">文章列表</span>
+    <span
+        :class="{gvb_tab: true, active: route.name === item.name}"
+        @click="clickTab(item)"
+        @click.middle="closeTab(item)"
+        v-for="item in tabList" :key="item.name">
+      {{ item.title }}
+      <IconClose @click.stop="closeTab(item)" v-if="item.name !== 'home'"></IconClose>
+    </span>
+    <span @click="closeAllTab" class="gvb_tab close_all_tab">全部关闭</span>
   </div>
 </template>
 
 <script setup lang="ts">
+import {IconClose} from "@arco-design/web-vue/es/icon";
+import type {Ref} from "vue";
+import {ref, watch} from "vue";
+import {useRoute, useRouter} from "vue-router";
+
+
+const route = useRoute()
+const router = useRouter()
+
+interface tabType {
+  name: string
+  title: string
+}
+
+const tabList: Ref<tabType[]> = ref([
+  {name: "home", title: "首页"},
+])
+
+
+function clickTab(item: tabType) {
+  router.push({name: item.name})
+}
+
+// 持久化存储
+function persistence() {
+  localStorage.setItem("tabList", JSON.stringify(tabList.value))
+}
+
+function loadTabs() {
+  let val = localStorage.getItem("tabList")
+  if (val === null) {
+    return
+  }
+  let tabs = []
+  try {
+    tabs = JSON.parse(val)
+  } catch (e) {
+    return;
+  }
+  tabList.value = tabs
+}
+
+loadTabs()
+watch(() => tabList.value.length, () => {
+  persistence()
+})
+
+function closeTab(item: tabType) {
+  // 点的home，不关
+  if (item.name === "home") {
+    return
+  }
+  // 找当前tab在列表里面的索引
+  let index = tabList.value.findIndex((tab) => item.name === tab.name)
+  tabList.value.splice(index, 1)
+  // 点的是当前页面的tab
+  // 判断删除的这个name是不是和我当前的路由是一样的
+  if (route.name === item.name) {
+    let beforeIndex = index - 1 // 因为首页一定会有
+    let beforeItem = tabList.value[beforeIndex]
+    clickTab(beforeItem)
+  }
+
+  // 点的是其他页面的tab
+}
+
+function closeAllTab() {
+  tabList.value = [
+    {name: "home", title: "首页"},
+  ]
+
+  router.push({name: "home"})
+}
+
+watch(() => route.name, () => {
+  if (!inList(route.name as string)) {
+    // 当前路由不在列表里面，那就将当前路由加到列表里面
+    tabList.value.push({
+      name: route.name as string,
+      title: route.meta.title as string,
+    })
+  }
+}, {immediate: true})
+
+
+function inList(name: string): boolean {
+  for (const tab of tabList.value) {
+    if (tab.name === name) {
+      return true
+    }
+  }
+  return false
+}
+
 
 </script>
 <style lang="scss">
@@ -17,6 +117,7 @@
   padding: 0 20px;
   display: flex;
   align-items: center;
+  position: relative;
 
   .gvb_tab {
     border-radius: 5px;
@@ -25,11 +126,31 @@
     margin-right: 10px;
     cursor: pointer;
 
+
     &.active {
       background-color: var(--active);
       color: white;
       border: none;
+
+      svg:hover {
+        background-color: rgb(var(--arcoblue-4));
+      }
     }
+
+    svg {
+      font-size: 12px;
+      border-radius: 50%;
+
+      &:hover {
+        background-color: var(--bg);
+      }
+    }
+  }
+
+  .close_all_tab {
+    position: absolute;
+    right: 20px;
+    margin-right: 0;
   }
 }
 </style>
